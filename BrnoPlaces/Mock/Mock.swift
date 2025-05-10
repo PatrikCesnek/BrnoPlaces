d40f86cbbeb13122e9775c6ea474833c04af0d89
Patrik Cesnek <pcesnek290@gmail.com>
2025-05-10 21:58:26 +0200

    Add mock data

--- BrnoPlaces/Mock/Mock.swift
+++ BrnoPlaces/Mock/Mock.swift
@@ -10,4 +10,15 @@ import MapKit
 
 struct Mock {
     static let mockLocation = CLLocationCoordinate2D(latitude: 16.5699853, longitude: 49.1971106)
+    
+    static let mockPlace = Place(
+        name: "Lorem",
+        placeDescription: mockDescription,
+        address: "MockAdress2025",
+        latitude: mockLocation.latitude,
+        longitude: mockLocation.longitude,
+        imageURL: ""
+    )
+    
+    static let mockDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
